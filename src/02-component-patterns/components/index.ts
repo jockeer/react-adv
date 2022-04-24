af3e799f0a95de1d002e8de1e43f80da163d7b7f
Daniel Gorianz <aalbaafikaa@gmail.com>
2022-04-24 09:46:57 -0400

    component compound

---
+++ src/02-component-patterns/components/index.ts
@@ -0,0 +1,18 @@
+import { ProductCard as  ProductCardHoc } from './ProductCard';
+
+import { ProductButtons } from "./ProductButtons";
+import { ProductImage } from "./ProductImage";
+import { ProductTitle } from "./ProductTitle";
+import { ProductCardHOCProps } from '../interfaces/intefaces';
+
+export { ProductButtons } from "./ProductButtons";
+export { ProductImage } from "./ProductImage";
+export { ProductTitle } from "./ProductTitle";
+
+export const ProductCard: ProductCardHOCProps = Object.assign( ProductCardHoc, {
+    Title: ProductTitle,
+    Image: ProductImage,
+    Buttons: ProductButtons
+})
+
+export default ProductCard
