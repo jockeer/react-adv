d98b35b4c8617cd89502a2d325d8c5edb684c0ff
Daniel Gorianz <aalbaafikaa@gmail.com>
2022-04-24 21:19:40 -0400

    component Extend Styles listo

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,9 +1,15 @@
 import noImage from '../assets/no-image.jpg'
-import { useContext } from 'react';
+import { CSSProperties, useContext } from 'react';
 import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css'
 
-export const ProductImage = ({img = ''}) => {
+export interface Props {
+    img?:string,
+    className?:string;
+    style?: CSSProperties
+}
+
+export const ProductImage = ({img, className, style}:Props) => {
     const {product} = useContext(ProductContext)
     let imgToShow:string;
     if (img) {
@@ -14,6 +20,6 @@ export const ProductImage = ({img = ''}) => {
         imgToShow = noImage
     }
     return (
-        <img className={ styles.productImg } src={imgToShow} alt="Product" />
+        <img className={ `${styles.productImg} ${className}` } src={imgToShow} alt="Product" style={style}/>
     )
 }
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,5 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
-
+import '../styles/custom-styles.css'
 const product = {
     id:'1',
     title:'Coffee Mug - Card',
@@ -15,16 +15,34 @@ export const ShoppingPage = () => {
             flexDirection:'row',
             flexWrap:'wrap'
         }}>
-            <ProductCard product={product}>
-              <ProductCard.Image />
-              <ProductCard.Title />
-              <ProductCard.Buttons />
+            <ProductCard product={product} className='bg-dark text-white'>
+              <ProductCard.Image className='custom-image'/>
+              <ProductCard.Title className='text-bold'/>
+              <ProductCard.Buttons className='custom-buttons'/>
             </ProductCard>
 
-            <ProductCard product={product}>
-              <ProductImage />
-              <ProductTitle title={'Cafe'}/>
-              <ProductButtons />
+            <ProductCard 
+              product={product}
+              className='bg-dark text-white'
+            >
+              <ProductImage className='custom-image'/>
+              <ProductTitle title={'Cafe'} className='text-bold'/>
+              <ProductButtons className='custom-buttons'/>
+            </ProductCard>
+
+            <ProductCard 
+              product={product}
+              style={{
+                backgroundColor:'#70d1f8'
+              }}
+             
+            >
+              <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}/>
+              <ProductTitle style={{ fontWeight:'bold'}}/>
+              <ProductButtons style={{
+                display:'flex',
+                justifyContent:'end'
+              }}/>
             </ProductCard>
 
         </div>
